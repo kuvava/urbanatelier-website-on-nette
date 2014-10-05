ccd384f4546eeea1ee517011e923a8e6d4b85701
Tomáš Urban <urbanovi@kuvava.cz>
2014-10-05 14:17:18 +0200

    Add managing `smazano` column in database `prispevek` table

--- app/model/MujPrispevek.php
+++ app/model/MujPrispevek.php
@@ -20,7 +20,8 @@ class MujPrispevek extends Nette\Object
 
 	/**
 	* 
-	* nejaký popis
+	* vloží podle příslušné url adresy do MujNaklad->prispevek příslušný řádek z databázové tabulky `prispevek`
+	* pokud pro daný MujNaklad->prispevek->smazano je hodnota 0, potom vrací pro celý MujNaklad->prispevek = FALSE, jakoby příslušný databázový řádek neexistoval...
 	* 
 	*/
 	public function nalozPrispevek($mujNakladak, $presenterId, $url1, $url2, $presenterName = NULL)
@@ -29,6 +30,9 @@ class MujPrispevek extends Nette\Object
 		$url2 = $url2 === NULL ? '' : $url2;
 		$presenterId = (($presenterId === NULL) || ((int)$presenterId < 1)) ? $this->database->table('presenter')->where('jmeno = ?', $presenterName)->min('id') : (int)$presenterId;
 		$mujNakladak->prispevek = $this->database->table('prispevek')->where('url1 = ?', $url1)->where('url2 = ?', $url2)->where('presenter_id = ?', $presenterId)->fetch();
+		if ($mujNakladak->prispevek->smazano > 0) {
+			$mujNakladak->prispevek = FALSE;
+		}
 	}
 
 }
